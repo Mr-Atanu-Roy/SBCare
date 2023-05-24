c2e2cf49daa84c1081b03134533b0fcea4d1efc9
Atanu <ar1995487@gmail.com>
2023-05-24 09:00:12 +0530

    completed url-short api

--- url_short_api/urls.py
+++ url_short_api/urls.py
@@ -4,5 +4,7 @@ from .views import *
 
 urlpatterns = [
     path("<token>", redirect_link, name="redirect_link"),
-    path("api/short-url/create", CreateShortURL.as_view(), name="create_short_url_api"),
+    path("api/short-url/", GetCreateShortURL.as_view(), name="get_create_short_url_api"),
+    path("api/short-url/<pk>", GetUpdateDeleteShortURL.as_view(), name="get_update_delete_short_url_api"),
+    
 ]
