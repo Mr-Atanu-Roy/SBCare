8557018986278341b8fbc20180c4d702e15ea88a
Atanu <ar1995487@gmail.com>
2023-05-27 00:22:19 +0530

    updated urls: removed white-spaces

--- qr_code_api/urls.py
+++ qr_code_api/urls.py
@@ -2,10 +2,7 @@ from django.urls import path
 
 from .views import *
 
-urlpatterns = [
-    #redirect url
-    # path("<token>", redirect_link, name="redirect_link"),
-    
+urlpatterns = [  
     #api urls
     path("api/qr-code/", GetCreateQR.as_view(), name="get_create_qr_api"),
     path("api/qr-code/<pk>", GetDeleteQR.as_view(), name="delete_qr_api"),
