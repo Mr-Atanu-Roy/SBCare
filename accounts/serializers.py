7cd5e7df6c8bc49688008583fa838f469b1cce08
Atanu <ar1995487@gmail.com>
2023-06-13 15:38:38 +0530

    updated UserTokenSerializer: removed id from read_only

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -5,10 +5,9 @@ from accounts.models import UserToken, User
 class UserTokenSerializer(ModelSerializer):
     
     class Meta:
-        fields = ['id', 'token', 'created_at']
+        fields = ['token', 'created_at']
         model = UserToken
         extra_kwargs = {
-            'id': {'read_only': True},
             'token': {'read_only': True},
             'created_at': {'read_only': True},
         }
